0077f281caa64d7f1d5ba0a87417d92078b68724
Administrator <s7130457@gmail.com>
2018-06-22 00:34:10 +0800

    first commit 設定好sign的router，要開始刻UI

---
+++ src/components/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="index">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><router-link to="/signin">Sign in</router-link></li>
+      <li><router-link to="/signup">Sign up</router-link></li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import index from '@/components/index'
+import signin from '@/components/signin'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: signin
+    }
+  ]
+})
